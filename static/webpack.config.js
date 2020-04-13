537885df0a00a65a16377ab8ba4d7c86200ff975
savvyyy <adityakumarpr@gmail.com>
2020-04-13 14:15:29 +0530

    Django React Boiler Plate

---
+++ static/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    watch: true,
+    devtool: 'source-map',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx'
+        ]
+    }
+}
